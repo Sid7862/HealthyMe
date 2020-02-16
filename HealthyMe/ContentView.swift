3d7093e078957386f5bb352a64e78c81c2a8cc85
Sahib <sahibuddin@gmail.com>
2020-02-16 11:14:48 +0530

    Initial Commit

---
+++ HealthyMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HealthyMe
+//
+//  Created by Ahmed Sahib on 16/02/20.
+//  Copyright © 2020 Ahmed Sahib. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
