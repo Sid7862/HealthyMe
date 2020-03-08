b0c351c7860202fe5a76dd57a59c427e6c863156
Ahmed Sahib <Sahibuddin@gmail.com>
2020-03-08 13:12:05 +0530

    changes

--- HealthyMe/MyUtils/Constants.swift
+++ HealthyMe/MyUtils/Constants.swift
@@ -6,6 +6,6 @@ class Constants {
     public static let encryptionKey: String = "౦༽౦ ‸ ౦༼౦"
     public static let genderOptions = ["🙍‍♂️ Male", "🙍‍♀️ Female", "🤖 Other"]
     public static let realationShip = ["Self","Father","Mother", "Sibling", "Grand Parent"]
-    public static let bloodGroup = ["A+","B+","A-", "B-", "AB+ AB-","O+","O-"]
+    public static let bloodGroup = ["A+","B+","A-", "B-", "AB+", "AB-","O+","O-"]
 
 }
