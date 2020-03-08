6fcf057953ee216c1a5d37dd33e89124c4d05694
Ahmed Sahib <Sahibuddin@gmail.com>
2020-03-08 13:44:15 +0530

    some changes

--- HealthyMe/Model/ProfileData.swift
+++ HealthyMe/Model/ProfileData.swift
@@ -10,19 +10,19 @@ import Foundation
 import RealmSwift
 
 final class ProfileData: ObservableObject {
-    @Published var profiles: [Profile]
+    @Published var profiles: [ProfileModel]
     private var profilesToken: NotificationToken?
 
 // Grab channels from Realm, and then activate a Realm token to listen for changes.
 init() {
     let realm = try! Realm()
-    profiles = Array(realm.objects(Profile.self)) // Convert Realm results object to Array
+    profiles = Array(realm.objects(ProfileModel.self)) // Convert Realm results object to Array
     activateprofilesToken()
 }
 
 private func activateprofilesToken() {
     let realm = try! Realm()
-    let profiles = realm.objects(Profile.self)
+    let profiles = realm.objects(ProfileModel.self)
     profilesToken = profiles.observe { _ in
         // When there is a change, replace the old channels array with a new one.
         self.profiles = Array(profiles)
